0cda474ba4ddee4f8227f76b620b20d6bb23e3f7
jltxseo <2529231020@qq.com>
2016-03-24 18:26:57 +0800

    添加Fragment动画

--- app/src/main/java/cn/xq/jltx/animationdemo/MainActivity.java
+++ app/src/main/java/cn/xq/jltx/animationdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package cn.xq.jltx.animationdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MainFragment mainFragment = MainFragment.newInstance(null,null);
+        fragmentTransaction.add(R.id.containt,mainFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
