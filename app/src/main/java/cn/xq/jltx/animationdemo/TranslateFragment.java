0ae12a3efc463292fa62498254fac5637fe70486
jltxseo <2529231020@qq.com>
2016-03-24 18:54:43 +0800

    添加Fragment动画

---
+++ app/src/main/java/cn/xq/jltx/animationdemo/TranslateFragment.java
@@ -0,0 +1,17 @@
+package cn.xq.jltx.animationdemo;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author jltxseo
+ *         Created by junlintianxia on 2016年03月24日.
+ */
+
+public class TranslateFragment extends Fragment {
+
+    public TranslateFragment() {
+    }
+
+
+}
